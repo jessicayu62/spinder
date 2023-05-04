2dc84c045654779354fd17d953aca001bfd1cc4b
Jessica Yu <jessicayu62@gmail.com>
2023-05-04 15:15:08 -0700

    add authorization for initial login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
+import config from './config.js'
+import Button from '@mui/material/Button';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
+  const url = `https://accounts.spotify.com/authorize?client_id=${config.CLIENT_ID}&response_type=code&redirect_uri=${config.REDIRECT_URI}`
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("/login")
+  //     // .then((res) => res.json())
+  //     // .then((data) => setData(data.message));
+  // }, []);
+  
+  const handleLogin = () => {
+    window.location.href = url;
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    console.log(code);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <Button onClick={handleLogin} variant="outlined">
+          Login
+        </Button>
       </header>
     </div>
   );
---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    CLIENT_ID : 'ae7630e04cd44f72a52549cdbba6b10b',
+    REDIRECT_URI : 'http://localhost:3000/callback',
+  }
+
+export default config;
